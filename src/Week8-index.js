f31598f8667fa8c71d2b5a83381ac1a472fcb06b
mlameb1 <mlameb1@gmail.com>
2023-08-17 16:27:45 +0200

    updated the html, js and css files

--- src/Week8-index.js
+++ src/Week8-index.js
@@ -1,51 +1,48 @@
-let currentDate = new Date();
-let h3 = document.querySelector("h3");
-let h4 = document.querySelector("h4");
-let date = currentDate.getDate();
-let hours = currentDate.getHours();
-if (hours < 10){hours =`0${hours}`};
-let minutes = currentDate.getMinutes();
-if (minutes < 10) {minutes = `0${minutes}`};
-let year = currentDate.getFullYear();
+function formatDate(timestamp) {
+  let currentDate = new Date(timestamp);
+  let date = currentDate.getDate();
+  let hours = currentDate.getHours();
+  if (hours < 10){hours =`0${hours}`};
+  let minutes = currentDate.getMinutes();
+  if (minutes < 10) {minutes = `0${minutes}`};
+  let year = currentDate.getFullYear();
+  let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+  ];
+  let day = days[currentDate.getDay()];
 
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[currentDate.getDay()];
+  let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let month = months[currentDate.getMonth()];
 
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-let month = months[currentDate.getMonth()];
+    return `${day}, ${date} ${month} ${year} ${hours}:${minutes}`;
+  }
 
-h3.innerHTML = `${day}, ${date} ${month} ${year}`;
-
-h4.innerHTML = `${hours}:${minutes}`;
-
-function formatDay(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-  return days[day];
-}
+  function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  
+    return days[day];
+  }
 
 function diplayForecast(response) {
   let forecast = response.data.daily;
@@ -54,36 +51,38 @@ function diplayForecast(response) {
   let forecastHTML = `<div class="row">`;
 
   forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
-      forecastHTML =
-        forecastHTML +
-        `
-            <div class="col-2">
-              <div class="weather-forecast-date">
-                ${formatDay(
+      if (index < 6) {
+          forecastHTML =
+              forecastHTML +
+              `
+          <div class="col-2">
+            <div class="weather-forecast-date">
+              ${formatDay(
                   forecastDay.dt
-                )} </div><img src="https://openweathermap.org/img/wn/${
-          forecastDay.weather[0].icon
-        }@2x.png" alt="" width="42px">
-               <div class="weather-forecast-temperatures">
-                 <span class="weather-forecast-temperature-max">${Math.round(
-                   forecastDay.temp.max
-                 )}°</span>
-                 <span class="weather-forecast-temperature-min">${Math.round(
-                   forecastDay.temp.min
-                 )}°</span>
-              </div>
+              )} </div><img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon
+              }@2x.png" alt="" width="42px">
+
+             <div class="weather-forecast-temperatures">
+               <span class="weather-forecast-temperature-max">${Math.round(
+                  forecastDay.temp.max
+              )}°</span>
+               <span class="weather-forecast-temperature-min">${Math.round(
+                  forecastDay.temp.min
+              )}°</span>
             </div>
-          `;
-    }
+          </div>
+        `;
+      }
   });
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
 
+
+  
 function getForecast(coordinates) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "a5acb752426cd8188485c35694980e3a";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(diplayForecast);
 }
@@ -91,22 +90,23 @@ function getForecast(coordinates) {
 
 
 function showWeather(response) {
-  h1= document.querySelector("h1");
+  
   let city = document.querySelector("#city");
   let humidity = document.querySelector("#hum");
   let wind = document.querySelector("#win");
   let temperature = document.querySelector("#temperature");
-  let h3 = document.querySelector("#date");
-  let h4 = document.querySelector("#time");
+  let date = document.querySelector("#date");
+  let icon = document.querySelector("#icon");
   celsiusTemperature = response.data.main.temp;
   temperature.innerHTML = `${Math.round(celsiusTemperature)}`;
   city.innerHTML = response.data.name;
-  h1.innerHTML = `${response.data.name}`;
+
   humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
   wind.innerHTML = `Wind speed: ${Math.round(response.data.wind.speed)} km/h`;
-  h3.innerHTML = `${day}, ${date} ${month} ${year}`;
-  h4.innerHTML = `${hours}:${minutes}`;
-getForecast(response.data.coordinates);
+  
+  date.innerHTML = formatDate(response.data.dt * 1000);
+  icon.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+getForecast(response.data.coord);
   
 }
 
@@ -151,7 +151,7 @@ function retrievePosition(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let apiKey = "a5acb752426cd8188485c35694980e3a";
-  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  let url =  `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
   console.log(url);
   axios.get(url).then(showWeather);
 }
